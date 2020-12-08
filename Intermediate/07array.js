d37f4fa387ff29fb097c7487756eb44e9d741c13
fasila <fasila@zmzevents.com>
2020-12-08 15:55:34 +0530

    commit

---
+++ Intermediate/07array.js
@@ -0,0 +1,21 @@
+var countrie = ["India", "USA", "JAPAN","rUSSIA"];
+
+var states = new Array("delhi", "mumbai","karnataka")
+
+//console.log(states[2]);
+//console.log(states.length);
+states[0] = "punjab";
+console.log(states);
+var user = ["fasila" , "fasilagmail.com", 3,33,true];
+console.log(user);
+
+user.pop();
+user.pop();
+console.log(user);
+
+user.unshift("New Value");
+console.log(user);
+user.shift();
+console.log(user);
+
+console.log(user.indexOf(3));
