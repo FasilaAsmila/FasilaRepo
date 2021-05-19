061984edc4ec0488aa47d7c881d787977f82bd85
fasila <fasila@zmzevents.com>
2021-05-19 12:42:02 +0530

    userstate

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,24 +1,33 @@
 import './App.css'
 import Header from './components/Header';
+import {useState} from 'react'
 
 function App() {
+
+  const [counter, setCount]=useState(0)
   const data = 'Asmila'
 
-  let count = 0;
+  // let counter = 0;
+  const addCount =()=>{
+    setCount(counter+1)
+    console.log(counter);
+  }
   return (
     <div>
-
-<h2>Counter: {count}</h2>
+  <button onClick={addCount}>Add</button>
+<h2>Counter: {counter}</h2>
 
 <Header data={data}/>
+
+  
 
 
     <p style={{color:'red'}}>Fasila {data} </p>
     
-    <Hello   />
+    {/* <Hello   />
     <Hello/>
     <Hello/>
-    
+     */}
 
     </div>
     
